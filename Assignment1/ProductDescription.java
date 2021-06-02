569ec30f2badbe916fee0385083cdcb23943ab7d
sueying <72725592+soonsueying278955@users.noreply.github.com>
2021-06-03 01:35:57 +0800

    Update ProductDescription.java

--- Assignment1/ProductDescription.java
+++ Assignment1/ProductDescription.java
@@ -51,9 +51,9 @@ public class ProductDescription {
 	private void productInfo() {
 		System.out.print("\n\n**********************************PRODUCT DESCRIPTION**********************************");
 		System.out.println("\nType of tourism and the service:" 
-				         + "\n1. Health Tourism.   Service : Cosmetic Surgery, Dentistry, Cardiac Surgery" 
-					     + "\n2. Sport Tourism.    Service : Snorkeling, Kayaking, Gym"
-					     + "\n3. Cultural Tourism. Service : Museums, Historic sites, Handicrafts");
+				 + "\n1. Health Tourism.   Service : Cosmetic Surgery, Dentistry, Cardiac Surgery" 
+				 + "\n2. Sport Tourism.    Service : Snorkeling, Kayaking, Gym"
+			         + "\n3. Cultural Tourism. Service : Museums, Historic sites, Handicrafts");
 		
 		System.out.print("\nPlease select the tourism you want : ");
 		tourism = scan.nextLine();
@@ -69,8 +69,8 @@ public class ProductDescription {
 		System.out.println("");
 		System.out.print("\n===============HEALTH TOURISM==============");
 		System.out.println("\n1. Price per person     : " + "RM 1000"  
-				         + "\n2. Professional quality : " + "Good, enthusiasm, politeness"
-				         + "\n3. ***If hour is more than 5 hours, you will get 20% discount");
+				 + "\n2. Professional quality : " + "Good, enthusiasm, politeness"
+				 + "\n3. ***If hour is more than 5 hours, you will get 20% discount");
 	}
 
 	private void sportTourism() {
@@ -84,9 +84,9 @@ public class ProductDescription {
 	private void culturalTourism() {
 		System.out.print("\n===============CULTURAL TOURISM=============");
 		System.out.println("\n1. Experience different cultures and beliefs"
-				         + "\n2. Learn about various custom"
-				         + "\n3. Enjoy tradisional food from all over the world"
-				         + "\n4. Price per person     : " + "RM 850"
-				         + "\n5. ***If hour is more than 5 hours, you will get 25% discount");
+				 + "\n2. Learn about various custom"
+				 + "\n3. Enjoy tradisional food from all over the world"
+				 + "\n4. Price per person     : " + "RM 850"
+				 + "\n5. ***If hour is more than 5 hours, you will get 25% discount");
 	}
 }
